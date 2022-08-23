12b1b0ac1780cfc63e42004cfc1a1f5742b015ce
hanzlasaadi <hanzalasaadi@gmail.com>
2022-08-23 16:49:16 +0500

    current location, display leaflet map

--- script.js
+++ script.js
@@ -10,3 +10,31 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+//Get current location from the browser
+//for old browsers
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(
+    function (position) {
+      console.log(position);
+      const { latitude, longitude } = position.coords;
+      console.log(latitude, longitude);
+
+      //Using Leaflet library to display a map
+      const map = L.map('map').setView([latitude, longitude], 13);
+      //"l" is a namespace just like {intl} that gives us certain methods such as map, tilelayer, marker
+      L.tileLayer('https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png', {
+        attribution:
+          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      }).addTo(map);
+
+      L.marker([latitude, longitude])
+        .addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+    },
+    function () {
+      alert('No position');
+    }
+  );
+}
